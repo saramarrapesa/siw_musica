aeff5b49061bc14555a0d99926df6a9eeb474d0b
saramarrapesa <115159885+saramarrapesa@users.noreply.github.com>
2023-09-18 21:44:57 +0200

    1

---
+++ src/main/java/it/uniroma3/siw/musica/MusicaApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.musica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicaApplication.class, args);
+	}
+
+}
